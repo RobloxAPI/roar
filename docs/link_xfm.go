e3bb876c2a50008cbf3c11d3b80ae5c299542c9b
Anaminus <anaminus@protonmail.com>
2024-09-12 13:01:44 -0500

    Move link transformation to separate method.

--- docs/link_xfm.go
+++ docs/link_xfm.go
@@ -13,6 +13,38 @@ type docLinkTransformer struct {
 	Context Context
 }
 
+func (t docLinkTransformer) transformLink(link string, image bool) (string, bool) {
+	u, err := url.Parse(link)
+	if err != nil {
+		return "", false
+	}
+	if u.Host == "" {
+		stem := u.Path
+		var x url.URL
+		if image {
+			x = t.Context.BaseImageURL
+			x.Path = path.Join(x.Path, stem)
+		} else {
+			ext := path.Ext(stem)
+			if ext == ".md" {
+				stem = stem[:len(stem)-len(ext)]
+				if path.Base(stem) == "index" {
+					stem = path.Dir(stem)
+				}
+			}
+			x = t.Context.BaseURL
+			if path.IsAbs(stem) {
+				x.Path = path.Join(x.Path, stem)
+			} else {
+				x.Path = path.Join(x.Path, path.Dir(t.Context.Path), stem)
+			}
+			x.Fragment = u.Fragment
+		}
+		return x.String(), true
+	}
+	return "", false
+}
+
 func (t docLinkTransformer) Transform(node *ast.Document, reader text.Reader, pc parser.Context) {
 	ast.Walk(node, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
 		if !entering {
@@ -20,39 +52,13 @@ func (t docLinkTransformer) Transform(node *ast.Document, reader text.Reader, pc
 		}
 		switch n := n.(type) {
 		case *ast.Link:
-			u, err := url.Parse(string(n.Destination))
-			if err != nil {
-				return ast.WalkSkipChildren, nil
-			}
-			if u.Host == "" {
-				stem := u.Path
-				ext := path.Ext(stem)
-				if ext == ".md" {
-					stem = stem[:len(stem)-len(ext)]
-					if path.Base(stem) == "index" {
-						stem = path.Dir(stem)
-					}
-				}
-				x := t.Context.BaseURL
-				if path.IsAbs(stem) {
-					x.Path = path.Join(x.Path, stem)
-				} else {
-					x.Path = path.Join(x.Path, path.Dir(t.Context.Path), stem)
-				}
-				x.Fragment = u.Fragment
-				n.Destination = []byte(x.String())
+			if href, ok := t.transformLink(string(n.Destination), false); ok {
+				n.Destination = []byte(href)
 			}
 			return ast.WalkSkipChildren, nil
 		case *ast.Image:
-			u, err := url.Parse(string(n.Destination))
-			if err != nil {
-				return ast.WalkSkipChildren, nil
-			}
-			if u.Host == "" {
-				stem := u.Path
-				x := t.Context.BaseImageURL
-				x.Path = path.Join(x.Path, stem)
-				n.Destination = []byte(x.String())
+			if href, ok := t.transformLink(string(n.Destination), true); ok {
+				n.Destination = []byte(href)
 			}
 			return ast.WalkSkipChildren, nil
 		}
